e01547c78b01df80f6a8cef4d82040f07be3a27b
Gustavo Felipe <gus1elsner@gmail.com>
2025-05-13 19:58:42 -0300

    demais arquivos do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { ListagemComponent} from './estoque/listagem/listagem.component';
+import { CadastroComponent } from './estoque/cadastro/cadastro.component';
+import { EdicaoComponent } from './estoque/edicao/edicao.component';
+export const routes: Routes = [
+{path:'', redirectTo: 'listagem', pathMatch: 'full'},
+{path: 'listagem', component:ListagemComponent},
+{path: 'cadastro', component:CadastroComponent},
+{path: 'edicao/:id', component:EdicaoComponent},
+];
